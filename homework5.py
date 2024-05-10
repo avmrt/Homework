11cc1ae428e520eff77a05ccee69477b30eb725a
avmrt <168987142+avmrt@users.noreply.github.com>
2024-05-10 13:35:22 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,14 @@
+my_list = ['banana', 'apple', 'pear', 'pineapple', 'melon']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'watermelon'
+print(my_list)
+
+
+my_dict = {'sword':'меч', 'spear':'копье', 'shield':'щит'}
+print(my_dict)
+print(my_dict['sword'])
+my_dict['sword'] = '1'
+print(my_dict)
