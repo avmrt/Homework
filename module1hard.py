e88c02feadd12e9c906ea0d94d2517a033435b4b
avmrt <168987142+avmrt@users.noreply.github.com>
2024-05-17 02:35:55 +0300

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,11 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+list_students = sorted(list(students))
+GPD = {}
+k=0
+
+for i in list_students:
+    GPD[i] = [sum(grades[k])/len(grades[k])]
+    k=k+1
+
+print(GPD)
