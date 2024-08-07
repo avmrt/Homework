e7e3b008211b6dd74bc17a8c3dc11c4afa9e8dbc
avmrt <168987142+avmrt@users.noreply.github.com>
2024-08-08 02:23:52 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,31 @@
+calls = 0
+str = ''
+list_of_str = []
+
+
+def count_calls(calls):
+    calls = calls + 1
+
+
+def string_info(str):
+    strbig = str.upper()
+    strlow = str.lower()
+    leng = len(str)
+    result = (leng, strbig, strlow)
+    count_calls(calls)
+    return result
+
+
+def is_contains(str, list_of_str):
+    count_calls(calls)
+    newlist = [i.lower() for i in list_of_str]
+    if str.lower() in newlist:
+        return True
+    else:
+        return False
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
